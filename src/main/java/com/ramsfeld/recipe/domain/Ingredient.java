f58ef74493672405bb58a0496d8b7ecda77cf1c4
KarkucinskiKrzysztof <ramsfeld.spam@gmail.com>
2019-05-17 23:33:14 +0200

    Add ingredient

---
+++ src/main/java/com/ramsfeld/recipe/domain/Ingredient.java
@@ -0,0 +1,54 @@
+package com.ramsfeld.recipe.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+// skladnik
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String description;
+    private BigDecimal amount;
+
+    //private UnitOfMeasure uom;
+
+    private Recipe recipe;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
