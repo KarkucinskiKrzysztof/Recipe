0a426f1f79a410bbd096f16dfe6a896e40313c47
KarkucinskiKrzysztof <ramsfeld.spam@gmail.com>
2019-09-25 23:57:29 +0200

    some test

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,21 +1,25 @@
 package guru.springframework.controllers;
 
 import guru.springframework.domain.Recipe;
-import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.services.RecipeService;
-import guru.springframework.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.ui.ConcurrentModel;
-import org.springframework.ui.ExtendedModelMap;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -24,22 +28,43 @@ public class IndexControllerTest {
     @Mock
     Model model;
 
-    IndexController indexController;
+    IndexController controller;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        indexController = new IndexController(recipeService);
-
+        controller = new IndexController(recipeService);
     }
 
     @Test
+    public void testMocMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+
+    @Test
     public void getIndexPage() {
+        // given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
 
-        assertEquals(indexController.getIndexPage(model),"index");
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);           // ciekawe ArgumentCaptor jako nazwa
+
+        // then
+        assertEquals(controller.getIndexPage(model),"index");
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
-
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());   // anySet() jeśli wywalimy wszytko z argumentarorem
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 
 }
